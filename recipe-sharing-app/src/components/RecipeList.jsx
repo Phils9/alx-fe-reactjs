5c46e284ae0fec8c19c9aeffc2d76994a786898e
Philip Cyprian <philip.thirdangle@gmail.com>
2025-03-03 15:35:11 +0100

    Updated RecipeList.jsx to use recipes state

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,11 +2,15 @@ import { Link } from 'react-router-dom';
 import useRecipeStore from './recipeStore'; // Ensure this import is correct
 
 const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+
+  // Use filteredRecipes if available, otherwise use recipes
+  const displayedRecipes = filteredRecipes.length > 0 ? filteredRecipes : recipes;
 
   return (
     <div>
-      {filteredRecipes.map((recipe) => (
+      {displayedRecipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>
             <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
