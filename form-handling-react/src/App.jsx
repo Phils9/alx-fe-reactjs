5633b9b1ff20387fa86af4aca8cc00b8dd92123f
Philip Olua <philip.thirdangle@gmail.com>
2025-03-07 00:50:00 +0100

    Add FormikForm component

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
 import FormikForm from './components/FormikForm';
 import './App.css';
-
 function App() {
   return (
     <div>
