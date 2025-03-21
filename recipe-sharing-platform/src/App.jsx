c0ee1604617fd035d63bbe6ad0b91b9137058e28
Phils9 <philip.thirdangle@gmail.com>
2025-03-21 23:52:46 +0100

    Initial setup: React app with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center text-blue-500">
+      <h1>Welcome to the Recipe Sharing Platform!</h1>
+    </div>
+  );
+}
+export default App;
